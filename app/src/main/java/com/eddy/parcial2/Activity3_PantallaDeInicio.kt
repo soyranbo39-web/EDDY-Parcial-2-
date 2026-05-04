5e63c8a8704145a39e458aa9a0c2d5c7b249db39
vicentt16 <vicentecvarguez@gmail.com>
2026-05-04 12:42:28 -0600

    ajustments

--- app/src/main/java/com/eddy/parcial2/Activity3_PantallaDeInicio.kt
+++ app/src/main/java/com/eddy/parcial2/Activity3_PantallaDeInicio.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.eddy.parcial2.databinding.ActivityMainBinding
 
-class Activity3 : AppCompatActivity() {
+class Activity3_PantallaDeInicio : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
