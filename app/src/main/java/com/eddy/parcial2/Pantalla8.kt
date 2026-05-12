9ebf68ab1f9889a780b28aa469a0a0236ba5903e
Pige <85260656+GPigE@users.noreply.github.com>
2026-05-11 18:47:19 -0600

    layout y formateado de dinero al escribir

--- app/src/main/java/com/eddy/parcial2/Pantalla8.kt
+++ app/src/main/java/com/eddy/parcial2/Pantalla8.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import java.text.NumberFormat
+import java.util.Locale
 
 class Pantalla8 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,36 @@ class Pantalla8 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val montoInput = findViewById<EditText>(R.id.inputMonto)
+        montoInput.addTextChangedListener(object : TextWatcher {
+            private var current = ""
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                if (s.toString() != current) {
+                    montoInput.removeTextChangedListener(this)
+                    val cleanString = s.toString()
+                        .replace("$", "")
+                        .replace(",", "")
+                        .replace(".", "")
+
+                    if (cleanString.isNotEmpty()) {
+                        val parsed = cleanString.toDouble() / 100
+                        val formatted = NumberFormat
+                            .getCurrencyInstance(Locale("es", "MX"))
+                            .format(parsed)
+
+                        current = formatted
+
+                        montoInput.setText(formatted)
+                        montoInput.setSelection(formatted.length)
+                    }
+
+                    montoInput.addTextChangedListener(this)
+                }
+            }
+        })
     }
 }
