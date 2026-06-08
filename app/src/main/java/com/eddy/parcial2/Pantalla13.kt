0f0c7e3cf5f0e411e74c1f5b53ad2fe844c83a7d
Pige <goldenpig.2709@gmail.com>
2026-06-08 05:42:49 -0600

    Pantalla 13

---
+++ app/src/main/java/com/eddy/parcial2/Pantalla13.kt
@@ -0,0 +1,28 @@
+package com.eddy.parcial2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Pantalla13 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pantalla13)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<ImageButton>(R.id.btnMenu).setOnClickListener {
+            startActivity(Intent(this, Pantalla3::class.java))
+            finish()
+        }
+    }
+}
